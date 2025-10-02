5cd648ebe6dc118153d6d93c2694fd2cb33afcb9
rishav_raj <145801777+rishav-wq@users.noreply.github.com>
2025-10-02 08:52:18 +0530

    Update apiClient.js

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000', // Your backend URL
+   baseURL: 'https://ai-quiz-hnh3.onrender.com', // Your backend URL
 });
 
 // This "interceptor" runs before every request
