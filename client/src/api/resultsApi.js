18e73d942d2a625c9d54eeeb35005afcdd10a583
rishav_raj <145801777+rishav-wq@users.noreply.github.com>
2025-10-02 09:03:10 +0530

    Update resultsApi.js

--- client/src/api/resultsApi.js
+++ client/src/api/resultsApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const getMyResultsAPI = async () => {
   try {
-    const response = await axios.get('/api/results');
+    const response = await axios.get('https://ai-quiz-hnh3.onrender.com/api/results');
     return response.data;
   } catch (error) {
     throw new Error('Failed to fetch quiz results.');
